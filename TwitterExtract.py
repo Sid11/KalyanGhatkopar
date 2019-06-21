e635f86f2fb07522bf85fd295acf91be3f31fee8
siddhesh <jsiddhesh96@gmail.com>
2019-06-21 17:09:59 +0530

    Added

---
+++ TwitterExtract.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import tweepy
+import csv
+import json
+
+# Open/create a file to append data to
+csvFile = open('result.csv', 'w')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+
+
+
+class TwitterExtract:
+
+
+
+    def __init__(self):
+
+        with open('event_credentials.json', 'r') as file:
+
+            self.credentials = json.load(file)
+
+        self.auth = tweepy.OAuthHandler(self.credentials['CONSUMER_KEY'], self.credentials['CONSUMER_SECRET'])
+
+        self.auth.set_access_token(self.credentials['ACCESS_TOKEN'], self.credentials['ACCESS_SECRET'])
+
+        self.api = tweepy.API(self.auth)
+
+        self.search_tweets = []
+
+
+
+    def extract_tweets(self, query='pulwama attack', max_tweets=100):
+
+        assert max_tweets <= 100
+
+        self.search_tweets = [status for status in tweepy.Cursor(self.api.search, q=query).items(max_tweets)]
+
+        with open('twitter_extraction.json', 'w') as file:
+
+            file.write('[')
+
+            for index, tweet in enumerate(self.search_tweets):
+
+                file.write(json.dumps(tweet._json) + (', ' if index != len(self.search_tweets) - 1 else ''))
+
+            file.write(']')
+       
+    def tocsv(self):	
+        for tweet in tweepy.Cursor(self.api.search,q = "pulwana attack",lang = "en").items(100):
+            # Write a row to the CSV file. I use encode UTF-8
+            csvWriter.writerow([tweet.text.encode('utf-8')])
+            print(tweet.entities)
+
+
+
+
+extractor = TwitterExtract()
+extractor.tocsv()
+extractor.extract_tweets() # Pass Parameters
---
+++ extractHash.py
@@ -0,0 +1,32 @@
+import csv
+import json
+import pandas as pd
+
+with open('twitter_extraction.json','r') as inputfile:
+    tweet= inputfile.read()
+
+tweet_json = json.loads(tweet)
+
+with open('output.csv', 'w') as csvfile:
+    fieldnames = ['created_at', 'user', 'hashtags']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    #extract all info that you want to write
+    #created_at = tweet_json["created_at"]
+    created_at = [x["created_at"] for x in tweet_json]
+    #selecting the screen_name of the user rather than id
+    #user = tweet_json["user"]["screen_name"]
+    #hashtags = tweet_json["entities"]["hashtags"]
+    user  = [x["user"]["screen_name"] for x in tweet_json]
+    hashtags = [x["entities"]["hashtags"] for x in tweet_json]
+    #creating an empty string for the hashtags in the array
+    hashes = list()
+    #print(hashtags)
+    for hashtag in hashtags:
+        #text = hashtag["text"]
+        text = [x["text"] for x in hashtag]
+        #append to hashes listed_count
+        hashes.append(text)
+    #stringify the list and write to file (will be ugly)
+    writer.writerow({"created_at":created_at, "user":user,"hashtags":str(hashes) })
+    
