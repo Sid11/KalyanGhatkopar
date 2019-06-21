fe04c47f8db5deaaedaa796b4de20c49ff4efb05
iraK21 <irakawthalkar21@gmail.com>
2019-06-21 17:51:49 +0530

    Changed again

--- TwitterExtract.py
+++ TwitterExtract.py
@@ -17,7 +17,7 @@ class TwitterExtract:
 
     def __init__(self):
         print("Yes")
-
+        print("No")
         with open('event_credentials.json', 'r') as file:
 
             self.credentials = json.load(file)
